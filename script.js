5889e19922766939dc8bf3a594b919af7233deb6
Jorge Netto <jorgenetto96@gmail.com>
2023-04-11 14:30:36 -0300

    Cria funcao randomComputerHand - Esta funcao gera um número aleatório de 1 a 3, depois o transforma em pedra, papel ou tesoura.

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-/*
-Funcao randomComputerHand() : Caractere
-Var
-   randomNumber: Inteiro
-Inicio
-      
-      randomNumber <- RandI(3)
-// Transforme o número gerado em texto
-   Se (randomNumber = 0) entao
-      retorne "pedra"
-   Senao
-        Se (randomNumber = 1) entao
-           retorne "papel"
-        Senao
-             Se (randomNumber = 2)entao
-                retorne "tesoura"
-             FimSe
-        FimSe
-   FimSe
-FimFuncao
-*/
-
-
 function randomComputerHand() {
-// função que gera um número aleatório de 0 a 2, sendo pedra [0],
-      //papel [1], tesoura [2]
-      let randomNumber = Math.floor(Math.random()*3) + 1;
-      return randomNumber;
-
+// função que gera um número aleatório de 0 a 2, sendo pedra [1],
+      //papel [2], tesoura [3]
+      let cpuHand;
+      let randomNumber = Math.floor(Math.random()*3) + 1; // Math.floor arrendonda pra baixo
+      if (randomNumber == 1) {
+         cpuHand = "pedra";
+      } else if (randomNumber == 2) {
+         cpuHand = "papel";
+      } else {
+         cpuHand = "tesoura"
+      }
+      return cpuHand;
 }
-console.log(randomComputerHand());
